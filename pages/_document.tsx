e48b4f0d340a263c3b7db8c48ea59de18db6f018
Nathan Romano <nathan.romano@hotmail.com>
2023-06-24 17:08:55 -0300

    title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head >
       <link rel="icon" href="/favicon32.png" />
+      <title>Obadog</title>
       </Head>
       <body>
         <Main />
